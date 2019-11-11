64c1c6a3662b8283279dcf804e535cb698254b25
Kevin Li <kevin.li@onsip.com>
2019-11-11 16:27:15 -0500

    Update JSON-schemas     - Now using draft v7     - Uses Ajv json validator     - Reformat schema

--- poc.js
+++ poc.js
@@ -1,39 +1,75 @@
 #!/usr/bin/env node
 
-const Validator = require('jsonschema').Validator;
+const Ajv = require('Ajv');
+const request = require("request-promise");
+
+const ajv = new Ajv({ allErrors: true, loadSchema: loadSchema });
+
+function loadSchema(uri) {
+  return request(uri).then(response => {
+    return JSON.parse(response);
+  }).catch(err => {
+    console.error("Fetching from uri: " + uri + " failed with: " + err);
+  });
+}
 
 const runRegister = () => {
-  const eventSchema = require("./schemas/sip/register.json");
-  const v = new Validator();
+  const uri = "https://raw.githubusercontent.com/onsip/push-notification-schema/dev-kevin-test/schemas/sip/register.json";
 
   const instance = {
     "data": {
-      "type": "sip.register",
+      "sip": {
+        "aor": "sip:grant@test.onsip.com",
+        "type": "sip.register",
+      },
       "payload": {
-        "aor": "sip:grant@test.onsip.com"
+        "method": "REGISTER",
+        "server": "edge.sip.onsip.com",
       }
     }
   };
-
-  console.log(v.validate(instance, eventSchema));
+  ajv.compileAsync({
+    $ref: uri
+  }).then(function (validate) {
+    var valid = validate(instance);
+    console.log("Register push is valid: " + valid);
+    if (!valid) {
+      validate.errors.forEach(error => {
+        console.log(error);
+      });
+    }
+  });
 };
 
 const runInvite = () => {
-  const eventSchema = require("./schemas/sip/invite.json");
-  const v = new Validator();
+  const uri = "https://raw.githubusercontent.com/onsip/push-notification-schema/dev-kevin-test/schemas/sip/invite.json";
 
   const instance = {
     "data": {
-      "type": "sip.invite",
-      "payload": {
+      "sip": {
+        "type": "sip.invite",
         "aor": "sip:grant@test.onsip.com",
+      },
+      "payload": {
+        "method": "INVITE",
         "server": "edge.sip.onsip.com",
-        "callid": "abc294dj349rgj"
+        "callid": "abc294dj349rgj",
+        "fromtag": "abc294dj349rgj"
       }
     }
   };
 
-  console.log(v.validate(instance, eventSchema));
+  ajv.compileAsync({
+    $ref: uri
+  }).then(function (validate) {
+    var valid = validate(instance);
+    console.log("Invite push is valid: " + valid);
+    if (!valid) {
+      validate.errors.forEach(error => {
+        console.log(error);
+      });
+    }
+  });
 };
 
 runRegister();
