f9c29305b241856ea09ba4a2730777918d01e28e
Kevin Li <kevin.li@onsip.com>
2019-11-12 09:47:28 -0500

    Update Tests and POC using AJV + new Schema

--- poc.js
+++ poc.js
@@ -3,17 +3,22 @@
 const Ajv = require('Ajv');
 const request = require("request-promise");
 
-const ajv = new Ajv({ allErrors: true, loadSchema: loadSchema });
+const baseSchema = require("./schemas/base.json");
 
 function loadSchema(uri) {
   return request(uri).then(response => {
-    return JSON.parse(response);
+    try {
+      return JSON.parse(response);
+    } catch (error) {
+      console.error(`Failed to parse: ${response} Error is ${error}`);
+    }
   }).catch(err => {
     console.error("Fetching from uri: " + uri + " failed with: " + err);
   });
 }
 
-const runRegister = () => {
+const runRegisterAsync = () => {
+  const ajv = new Ajv({ allErrors: true, loadSchema: loadSchema });
   const uri = "https://raw.githubusercontent.com/onsip/push-notification-schema/dev-kevin-test/schemas/sip/register.json";
 
   const instance = {
@@ -32,7 +37,7 @@ const runRegister = () => {
     $ref: uri
   }).then(function (validate) {
     var valid = validate(instance);
-    console.log("Register push is valid: " + valid);
+    console.log("Async Register push is valid: " + valid);
     if (!valid) {
       validate.errors.forEach(error => {
         console.log(error);
@@ -41,7 +46,8 @@ const runRegister = () => {
   });
 };
 
-const runInvite = () => {
+const runInviteAsync = () => {
+  const ajv = new Ajv({ allErrors: true, loadSchema: loadSchema });
   const uri = "https://raw.githubusercontent.com/onsip/push-notification-schema/dev-kevin-test/schemas/sip/invite.json";
 
   const instance = {
@@ -63,7 +69,7 @@ const runInvite = () => {
     $ref: uri
   }).then(function (validate) {
     var valid = validate(instance);
-    console.log("Invite push is valid: " + valid);
+    console.log("Async Invite push is valid: " + valid);
     if (!valid) {
       validate.errors.forEach(error => {
         console.log(error);
@@ -71,6 +77,65 @@ const runInvite = () => {
     }
   });
 };
+const registerSchema = require("./schemas/sip/register.json");
 
-runRegister();
-runInvite();
+const runInviteSync = () => {
+  const ajv = new Ajv({ allErrors: true });
+
+  const inviteSchema = require("./schemas/sip/invite.json");
+
+  const instance = {
+    "data": {
+      "sip": {
+        "type": "sip.invite",
+        "aor": "sip:grant@test.onsip.com",
+      },
+      "payload": {
+        "method": "INVITE",
+        "server": "edge.sip.onsip.com",
+        "callid": "abc294dj349rgj",
+        "fromtag": "abc294dj349rgj"
+      }
+    }
+  };
+  var validate = ajv.addSchema(baseSchema).compile(inviteSchema);
+  var valid = validate(instance);
+  console.log("Synchronous Invite push is valid: " + valid);
+  if (!valid) {
+    validate.errors.forEach(error => {
+      console.log(error);
+    });
+  }
+};
+
+const runRegisterSync = () => {
+  const ajv = new Ajv({ allErrors: true });
+  const registerSchema = require("./schemas/sip/register.json");
+
+  const instance = {
+    "data": {
+      "sip": {
+        "aor": "sip:grant@test.onsip.com",
+        "type": "sip.register",
+      },
+      "payload": {
+        "method": "REGISTER",
+        "server": "edge.sip.onsip.com",
+      }
+    }
+  };
+
+  var validate = ajv.addSchema(baseSchema).compile(registerSchema);
+  var valid = validate(instance);
+  console.log("Synchronous Register push is valid: " + valid);
+  if (!valid) {
+    validate.errors.forEach(error => {
+      console.log(error);
+    });
+  }
+};
+
+runRegisterAsync();
+runRegisterSync();
+runInviteAsync();
+runInviteSync();
--- spec/schemaSpec.js
+++ spec/schemaSpec.js
@@ -1,117 +1,142 @@
-const jsonschema = require('jsonschema');
-const Validator = jsonschema.Validator;
+const Ajv = require('Ajv');
+const baseSchema = require("../schemas/base.json");
+const registerSchema = require("../schemas/sip/register.json");
+const inviteSchema = require("../schemas/sip/invite.json");
 
-let v;
+let ajv;
+let validate;
 let instance;
 
-describe('notification', function() {
-  describe('register', function() {
-
-    const notificationSchema = require("../schemas/sip/register.json");
+describe('notification', function () {
+  describe('register', function () {
     beforeEach(() => {
-      v = new Validator();
+      ajv = new Ajv({ allErrors: true })
+      validate = ajv.addSchema(baseSchema).compile(registerSchema);
+
       instance = {
         "data": {
-          "type": "sip.register",
-          "payload": {
+          "sip": {
             "aor": "sip:grant@test.onsip.com",
+            "type": "sip.register",
+          },
+          "payload": {
+            "method": "REGISTER",
+            "server": "edge.sip.onsip.com",
           }
         }
       };
     });
 
     it('schema is a valid schema', () => {
-      const emptyValidate = () => {
-        v.validate({}, notificationSchema);
-      }
-      expect(emptyValidate).not.toThrowError(jsonschema.SchemaError);
+      const result = ajv.validateSchema(baseSchema) &&
+        ajv.validateSchema(registerSchema);
+      expect(result).toBe(true);
     });
 
     it('instance should match schema exactly', () => {
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length).toBe(0);
+      const result = validate(instance);
+      expect(result).toBe(true);
     });
 
     it('instance should pass validation, has extra data field', () => {
       instance["data"]["extra"] = "gravy!";
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length).toBe(0);
+      const result = validate(instance);
+      expect(result).toBe(true);
     });
 
     it('instance should pass validation, has extra sip field', () => {
       instance["data"]["payload"]["extra"] = "gravy!";
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length).toBe(0);
+      const result = validate(instance);
+      expect(result).toBe(true);
     });
 
-    ["aor"].forEach((key) => {
+    ["aor", "type"].forEach((key) => {
+      it('instance should fail validation, missing ' + key, () => {
+        delete instance["data"]["sip"][key];
+        const result = validate(instance);
+        expect(result).toBe(false);
+      });
+    });
+
+    ["method", "server"].forEach((key) => {
       it('instance should fail validation, missing ' + key, () => {
         delete instance["data"]["payload"][key];
-        const result = v.validate(instance, notificationSchema);
-        expect(result.errors.length > 0).toBeTruthy();
+        const result = validate(instance);
+        expect(result).toBe(false);
       });
     });
 
     it('instance should fail validation, missing payload', () => {
       delete instance["data"]["payload"];
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length > 0).toBeTruthy();
+      const result = validate(instance);
+      expect(result).toBe(false);
     });
   });
 
   describe('invite', () => {
-
-    const notificationSchema = require("../schemas/sip/invite.json");
     beforeEach(() => {
-      v = new Validator();
+      ajv = new Ajv({ allErrors: true })
+      validate = ajv.addSchema(baseSchema).compile(inviteSchema);
+
       instance = {
         "data": {
-          "type": "sip.invite",
-          "payload": {
+          "sip": {
+            "type": "sip.invite",
             "aor": "sip:grant@test.onsip.com",
+          },
+          "payload": {
+            "method": "INVITE",
             "server": "edge.sip.onsip.com",
-            "callid": "abc294dj349rgj"
+            "callid": "abc294dj349rgj",
+            "fromtag": "abc294dj349rgj"
           }
         }
       };
     });
 
     it('schema is a valid schema', () => {
-      const emptyValidate = () => {
-        v.validate({}, notificationSchema);
-      };
-      expect(emptyValidate).not.toThrowError(jsonschema.SchemaError);
-    });
+      const result = ajv.validateSchema(baseSchema) &&
+        ajv.validateSchema(inviteSchema);
+      expect(result).toBe(true);
+    });;
 
     it('instance should match schema exactly', () => {
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length).toBe(0);
+      const result = validate(instance);
+      expect(result).toBe(true);
     });
 
     it('instance should pass validation, has extra data field', () => {
       instance["data"]["extra"] = "gravy!";
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length).toBe(0);
+      const result = validate(instance);
+      expect(result).toBe(true);
     });
 
     it('instance should pass validation, has extra payload field', () => {
       instance["data"]["payload"]["extra"] = "gravy!";
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length).toBe(0);
+      const result = validate(instance);
+      expect(result).toBe(true);
     });
 
-    ["aor", "server", "callid"].forEach((key) => {
+    ["aor", "type"].forEach((key) => {
       it('instance should fail validation, missing ' + key, () => {
-        delete instance["data"]["payload"][key];
-        const result = v.validate(instance, notificationSchema);
-        expect(result.errors.length > 0).toBeTruthy();
+        delete instance["data"]["sip"][key];
+        const result = validate(instance);
+        expect(result).toBe(false);
       });
     });
 
-    it('instance should fail validation, missing payload', function() {
+    ["method", "server", "callid", "fromtag"].forEach((key) => {
+      it('instance should fail validation, missing ' + key, () => {
+        delete instance["data"]["payload"][key];
+        const result = validate(instance);
+        expect(result).toBe(false);
+      });
+    });
+
+    it('instance should fail validation, missing payload', function () {
       delete instance["data"]["payload"];
-      const result = v.validate(instance, notificationSchema);
-      expect(result.errors.length > 0).toBeTruthy();
+      const result = validate(instance);
+      expect(result).toBe(false);
     });
   });
 });
